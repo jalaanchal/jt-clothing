a8abf8d59aa0b8d1db548a198c4285bff3f4a26d
jalaanchal <jalaanchal@gmail.com>
2023-03-21 18:52:41 +0530

    created authentication and sign-in-form components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -57,7 +57,7 @@ const SignUpForm = () => {
   return (
     <div className="sign-up-container">
       <h2>Don't have an account?</h2>
-      <span>Sign up with your email and password.</span>
+      <span>Sign up for a new account.</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -91,7 +91,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <Button buttonType="google" type="submit">Sign Up</Button>
+        <Button buttonType="inverted" type="submit">Sign Up</Button>
       </form>
     </div>
   );
